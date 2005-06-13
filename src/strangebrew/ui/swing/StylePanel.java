f1badafe7b5f80106a8defb31755863ee75e3875
andrew_avis <andrew_avis>
2005-06-13 18:56:51 +0000

    Update

--- src/strangebrew/ui/swing/StylePanel.java
+++ src/strangebrew/ui/swing/StylePanel.java
@@ -25,22 +25,6 @@ import javax.swing.border.TitledBorder;
 import strangebrew.Recipe;
 import strangebrew.Style;
 
-
-
-/**
-* This code was generated using CloudGarden's Jigloo
-* SWT/Swing GUI Builder, which is free for non-commercial
-* use. If Jigloo is being used commercially (ie, by a corporation,
-* company or business for any purpose whatever) then you
-* should purchase a license for each developer using Jigloo.
-* Please visit www.cloudgarden.com for details.
-* Use of Jigloo implies acceptance of these licensing terms.
-* *************************************
-* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED
-* for this machine, so Jigloo or this code cannot be used legally
-* for any corporate or commercial purpose.
-* *************************************
-*/
 public class StylePanel extends javax.swing.JPanel {
 	private JLabel jLabel1;
 	private JLabel lblStyle2;
