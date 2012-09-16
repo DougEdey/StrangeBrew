26a38360bf9adcd043184da0b46a10f09cb6127c
dougedey <doug.edey@gmail.com>
2012-09-16 17:22:39 -0400

    Forgot to upload the new files...

---
+++ src/ca/strangebrew/ui/swing/PantryFermTableModel.java
@@ -0,0 +1,195 @@
+
+
+/*
+ * Created on May 3, 2005
+ */
+package ca.strangebrew.ui.swing;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import javax.swing.table.AbstractTableModel;
+
+import ca.strangebrew.Debug;
+import ca.strangebrew.Fermentable;
+
+import ca.strangebrew.SBStringUtils;
+
+
+public class PantryFermTableModel extends AbstractTableModel {
+	
+	static final private String[] columnNames = {"S", "M", "Malt", "Stock", "Units", "Points",
+			"Lov", "Cost/U", "%"};
+	
+	public List<Fermentable> data = new ArrayList<Fermentable>();
+
+	
+
+	public PantryFermTableModel() {
+		
+	}
+
+	public void addRow(Fermentable m) {
+		data.add(m);
+	}
+
+	public void setData(List<Fermentable> d) {
+		data = d;
+	}
+
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	public int getRowCount() {
+		if (data != null)
+			return data.size();
+		else
+			return 0;
+	}
+
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
+	
+	public String getDescriptionAt(int row) {
+		if (row < 0)
+			return "";
+		return data.get(row).getDescription();
+	}
+
+	public Object getValueAt(int row, int col) {
+		// Fermentable m = (Fermentable) data.get(row);
+		try {
+			switch (col) {
+				case 0: // steep
+					return new Boolean(data.get(row).getSteep());
+				case 1: // mash
+					return new Boolean (data.get(row).getMashed());
+				case 2 :					
+					return data.get(row).getName();
+				case 3 :
+					
+					return SBStringUtils.format(data.get(row)
+							.getStockAs(data.get(row).getUnits()), 2);
+				case 4 :
+					return data.get(row).getUnits();
+				case 5 :
+					return SBStringUtils.format(data.get(row).getPppg(), 3);
+				case 6 :
+					return SBStringUtils.format(data.get(row).getLov(), 0);
+				case 7 :
+					
+					return new Double(data.get(row).getCostPerU());
+				
+			}
+		} catch (Exception e) {
+		};
+		return "";
+	}
+
+	/*
+	 * JTable uses this method to determine the default renderer/ editor for
+	 * each cell. If we didn't implement this method, then the last column
+	 * would contain text ("true"/"false"), rather than a check box.
+	 */
+	
+	public Class<?> getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's editable.
+	 */
+	public boolean isCellEditable(int row, int col) {
+		//Note that the data/cell address is constant,
+		//no matter where the cell appears onscreen.
+		if (col < 0 || col == 2) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's data can
+	 * change.
+	 */
+	public void setValueAt(Object value, int row, int col) {
+		Debug.print("Setting at " + row + "/" + col);
+		// Fermentable m = (Fermentable) data.get(row);
+		try {
+			switch (col) {
+				case 0: 
+					data.get(row).setSteep(new Boolean (value.toString()).booleanValue());
+					break;
+				case 1:
+					data.get(row).setMashed(new Boolean(value.toString()).booleanValue());
+					break;
+				case 2 :
+					
+					data.get(row).setName(value.toString());
+					
+					// Shouldn't this re-set most of the data fields with base info
+					if (value instanceof Fermentable) {
+						Fermentable m = (Fermentable)value;
+						data.get(row).setPppg(m.getPppg());
+						data.get(row).setLov(m.getLov());
+						data.get(row).setSteep(m.getSteep());
+						data.get(row).setMashed(m.getMashed());						
+					}
+					break;
+				case 3 :
+					try {
+						Debug.print("Setting Stock to: "+value.toString());
+						NumberFormat format = NumberFormat.getInstance(Locale.getDefault());
+						Number number = format.parse(value.toString().trim());
+						data.get(row).setStock(number.doubleValue());
+						Debug.print("Stock set to: " + data.get(row).getStockAs(data.get(row).getUnits()));
+					} catch (NumberFormatException m) {
+						Debug.print("Could not parse "+ value.toString() + " as a double");
+
+					}
+					//sort by the malt amount	
+					
+					break;
+				case 4 :
+					 data.get(row).setUnits(value.toString());
+					break;
+				case 5 :
+					try {
+						NumberFormat format = NumberFormat.getInstance(Locale.getDefault());
+						Number number = format.parse(value.toString().trim());
+						data.get(row).setPppg(number.doubleValue());
+					} catch (NumberFormatException m) {
+						Debug.print("Could not parse "+ value.toString() + " as a double");
+
+					}
+					break;
+				case 6 :
+					try {
+						NumberFormat format = NumberFormat.getInstance(Locale.getDefault());
+						Number number = format.parse(value.toString().trim());
+						data.get(row).setLov(number.doubleValue());
+					} catch (NumberFormatException m) {
+						Debug.print("Could not parse "+ value.toString() + " as a double");
+
+					}
+					break;
+				case 7 :
+					
+					data.get(row).setCost(value.toString());
+					break;
+				
+
+			}
+		} catch (Exception e) {
+		};
+		
+		fireTableCellUpdated(row, col);
+		fireTableDataChanged();		
+		
+	}
+}
---
+++ src/ca/strangebrew/ui/swing/dialogs/RemoteRecipes.java
@@ -0,0 +1,449 @@
+/*
+ * $Id: FindDialog.java,v 1.5 2008/01/19 01:05:40 jimcdiver Exp $ 
+ * Created on June 15, 2005 @author aavis find recipe window class
+ */
+
+/**
+ *  StrangeBrew Java - a homebrew recipe calculator
+ Copyright (C) 2005  Drew Avis
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package ca.strangebrew.ui.swing.dialogs;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.AbstractTableModel;
+
+import ca.strangebrew.Debug;
+import ca.strangebrew.OpenImport;
+import ca.strangebrew.Options;
+import ca.strangebrew.Recipe;
+import ca.strangebrew.SBStringUtils;
+import ca.strangebrew.ui.swing.StrangeSwing;
+
+public class RemoteRecipes extends javax.swing.JDialog implements ActionListener {
+	private JPanel findPanel;
+	private JButton browseButton;
+	private JTextField dirLocationText;
+	private JPanel browsePanel;
+	private JButton cancelButton;
+	private JButton openButton;
+	private JPanel buttonPanel;
+	private JTable recipeTable;
+	private JScrollPane recipeScrollPane;
+	private FindTableModel recipeTableModel;
+	private Options opt;
+
+	private List<BasicRecipe> recipes = new ArrayList<BasicRecipe>();
+	private List<File> files = new ArrayList<File>();
+	private File currentDir;
+	private boolean close = false;
+	
+	public RemoteRecipes(JFrame frame) {
+		super(frame);
+		
+		
+		String recipeDir = "";
+		opt = Options.getInstance();
+		if(opt.getProperty("optRecipe") != null)
+			recipeDir = opt.getProperty("optRecipe");
+		
+		if(recipeDir.equalsIgnoreCase("") ) {
+			try {
+				recipeDir = SBStringUtils.getAppPath("recipes");
+			} catch (UnsupportedEncodingException e) {
+				// 
+				JOptionPane.showMessageDialog(frame, "Couldn't get the App Path for the find recipe dialog.");
+			}
+		}
+		currentDir = new File(recipeDir);
+		
+		initGUI();
+		dirLocationText.setText(currentDir.getAbsolutePath());
+		loadRecipes(currentDir);
+		
+		
+	}
+
+	private void initGUI() {
+		try {
+			GridBagLayout thisLayout = new GridBagLayout();
+			thisLayout.columnWeights = new double[]{0.1};
+			thisLayout.columnWidths = new int[]{7};
+			thisLayout.rowWeights = new double[]{0.7, 0.1, 0.1};
+			thisLayout.rowHeights = new int[]{7, 7, 7};
+			this.getContentPane().setLayout(thisLayout);
+			{
+				findPanel = new JPanel();
+				this.getContentPane().add(
+						findPanel,
+						new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH,
+								GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+				BoxLayout findPanelLayout = new BoxLayout(findPanel, javax.swing.BoxLayout.X_AXIS);
+				findPanel.setLayout(findPanelLayout);
+				findPanel.setPreferredSize(new java.awt.Dimension(392, 269));
+				{
+					recipeScrollPane = new JScrollPane();
+					findPanel.add(recipeScrollPane);
+					// recipeScrollPane.setPreferredSize(new
+					// java.awt.Dimension(152, 75));
+					{
+						recipeTableModel = new FindTableModel();
+						recipeTable = new JTable();
+						recipeScrollPane.setViewportView(recipeTable);
+						recipeTable.setModel(recipeTableModel);
+						// recipeTable.setPreferredSize(new
+						// java.awt.Dimension(148, 32));
+					}
+				}
+			}
+			{
+				browsePanel = new JPanel();
+				this.getContentPane().add(
+						browsePanel,
+						new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
+								GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+				BorderLayout browsePanelLayout = new BorderLayout();
+				browsePanel.setLayout(browsePanelLayout);
+				browsePanel.setBorder(BorderFactory.createTitledBorder("Directory"));
+				browsePanel.setPreferredSize(new java.awt.Dimension(392, 244));
+				{
+					dirLocationText = new JTextField();
+					browsePanel.add(dirLocationText, BorderLayout.CENTER);
+					dirLocationText.setText("jTextField1");
+				}
+				{
+					browseButton = new JButton();
+					browsePanel.add(browseButton, BorderLayout.EAST);
+					browseButton.setText("Browse...");
+					browseButton.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent evt) {
+							Component owner = browseButton;
+							JFileChooser chooser = new JFileChooser();
+							chooser.setCurrentDirectory(new java.io.File("."));
+							chooser.setDialogTitle("Select directory");
+							chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+							if (chooser.showOpenDialog(owner) == JFileChooser.APPROVE_OPTION) {
+								Debug.print("getCurrentDirectory(): "
+										+ chooser.getCurrentDirectory());
+								Debug.print("getSelectedFile() : " + chooser.getSelectedFile());
+								currentDir = chooser.getSelectedFile();
+								dirLocationText.setText(currentDir.getAbsolutePath());
+								loadRecipes(currentDir);
+							} else {
+								Debug.print("No Selection ");
+							}
+						}
+					});
+				}
+			}
+			{
+				buttonPanel = new JPanel();
+				this.getContentPane().add(
+						buttonPanel,
+						new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
+								GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+				FlowLayout buttonPanelLayout = new FlowLayout();
+				buttonPanelLayout.setAlignment(FlowLayout.RIGHT);
+				buttonPanel.setLayout(buttonPanelLayout);
+				{
+					openButton = new JButton();
+					buttonPanel.add(openButton);
+					openButton.setText("Open");
+					openButton.addActionListener(this);
+				}
+				{
+					cancelButton = new JButton();
+					buttonPanel.add(cancelButton);
+					cancelButton.setText("Cancel");
+					cancelButton.addActionListener(this);
+				}
+			}
+			this.setSize(400, 428);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void loadRecipes(File dir) {
+
+		// Reads the list of recipes on the remote Database
+		Connection conn = null;
+
+	    try
+	    {
+	
+	        String url = "jdbc:mysql://mysql1.gohsphere.com:3306/gmnoel_strangebrew";
+	        Class.forName ("com.mysql.jdbc.Driver");
+	        conn = DriverManager.getConnection (url,"gmnoel_strange","brew1234");
+	        //System.out.println ("Database connection established");
+	    
+	        Statement query = conn.createStatement();
+	        ResultSet allRecipesQuery;
+	        if(query.execute("SELECT ID, Brewer, Title, Style, Iteration FROM recipes;")) {
+	        
+	        	allRecipesQuery = query.getResultSet();
+	        	if(!allRecipesQuery.last()) {
+	        		JOptionPane.showMessageDialog(null, "No Recipes found in the online database!");
+	        		conn.close();
+	        		this.close = true;
+	        		return;
+	        	}
+	        	
+	        	allRecipesQuery.first();
+	        	
+	        } else { 
+	        	
+	        	//System.out.println("could not get recipes!");
+	        	return;
+	        }
+		    
+			//List<BasicRecipe> recipes = new ArrayList<BasicRecipe>();
+			Debug.print("Loading recipes from online");
+			do {
+				
+				int ID = allRecipesQuery.getInt("ID");
+				String Brewer = allRecipesQuery.getString("Brewer");
+				String Title = allRecipesQuery.getString("Title");
+				String Style = allRecipesQuery.getString("Style");
+				int iteration = allRecipesQuery.getInt("Iteration");
+				Debug.print("Loading: " + Title);
+				BasicRecipe rRecipe = new BasicRecipe(ID, Brewer, Style, Title, iteration);
+				recipes.add(rRecipe);
+				
+			} while(allRecipesQuery.next());
+			recipeTableModel.setData(recipes);
+			recipeTable.updateUI();
+	    }
+	    catch (Exception e)
+	    {
+	        e.printStackTrace();
+	
+	    }
+		// done with the connection, close it
+        if (conn != null)
+        {
+            try
+            {
+                conn.close ();
+                //System.out.println ("Database connection terminated");
+            }
+            catch (Exception e) { /* ignore close errors */ }
+        }
+
+	}
+	
+	public void focusGained() {
+		
+	}
+	public void actionPerformed(ActionEvent e) {
+		if(close) {
+			Debug.print("Closing window");
+			RemoteRecipes.this.setVisible(true);
+			//System.out.println(RemoteRecipes.this.toString());
+			RemoteRecipes.this.setVisible(false);
+			RemoteRecipes.this.dispose();
+			return;
+		}
+		
+		Object o = e.getSource();
+		if (o == cancelButton) {
+			//System.out.println(this.toString());
+			setVisible(false);
+			dispose();
+			
+			return;
+		} else if (o == openButton) {
+			
+			
+			int i = recipeTable.getSelectedRow();
+			Debug.print("Opening row " + i + " - " + recipes.size());
+			if (i > -1 && i < recipes.size()) {
+				// grab the Blob and name
+				int ID = recipes.get(i).id;
+				StrangeSwing.getInstance().recipeFile = getRemoteRecipe(ID);
+				// open the file to write to
+				
+			}
+
+			setVisible(false);
+			dispose();
+			return;
+		}
+	}
+
+	private String getRemoteRecipe(int ID) {
+		try
+	    {
+	
+	        String url = "jdbc:mysql://mysql1.gohsphere.com:3306/gmnoel_strangebrew";
+	        Class.forName ("com.mysql.jdbc.Driver");
+	        Connection conn = DriverManager.getConnection (url,"gmnoel_strange","brew1234");
+	        //System.out.println ("Database connection established");
+	    
+	        PreparedStatement query = conn.prepareStatement("SELECT  Brewer, Title, Style, Iteration, Recipe FROM recipes WHERE ID = ?;");
+	        query.setInt(1, ID);
+	        ResultSet allRecipesQuery = query.executeQuery();
+	        if(allRecipesQuery.last()) {
+	        	
+	        	allRecipesQuery.first();
+	        	Blob recipe = allRecipesQuery.getBlob("Recipe");
+	        	String Title = allRecipesQuery.getString("Title");
+	        	String Brewer = allRecipesQuery.getString("Brewer");
+	        	int iteration = allRecipesQuery.getInt("Iteration");
+	        	
+	        	
+	        	// check if we already have this file
+	        	String file = Title + " - " + Brewer + " ("+ iteration + ").xml";
+	        	
+	        	File recipeFile = new File(currentDir, file);
+	        	
+	        	if(recipeFile.exists()) {
+	        		
+	        		JOptionPane.showMessageDialog(null, "This file already exists!");
+	        		return recipeFile.getAbsolutePath();
+	        		
+	        	}
+	        	Debug.print("Writing recipe file: " + recipeFile.getAbsolutePath());
+	        	// file doesn't exist, lets dump the file 
+	        	OutputStream oStream = new FileOutputStream(recipeFile);
+	        	oStream.write(recipe.getBytes(1, (int) recipe.length()));
+	        	oStream.close();
+	        	return recipeFile.getAbsolutePath();
+	        } else { 
+	        	JOptionPane.showMessageDialog(null, "Couldn't find the selected recipe (ID "+ ID +")");
+        		conn.close();
+        		this.close = true;
+        		return ""	;
+	        	
+	        }
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    	return "";
+	    }
+	}
+	
+	class BasicRecipe {
+		
+		public int id;
+		public String brewer;
+		public String style;
+		public String title;
+		public int iteration;
+		
+		BasicRecipe(int ID, String Brewer, String Type, String Title, int Iteration) {
+			id = ID;
+			brewer = Brewer;
+			style = Type;
+			title = Title;
+			iteration = Iteration;
+		}
+	}
+	
+	class FindTableModel extends AbstractTableModel {
+
+		private String[] columnNames = {"Recipe", "Style", "Brewer", "Iteration"};
+
+		private List<BasicRecipe> data;
+
+		public FindTableModel() {
+			// data = new ArrayList();
+		}
+
+		public void setData(List<BasicRecipe> l) {
+			data = l;
+		}
+
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		public int getRowCount() {
+			if (data != null)
+				return data.size();
+			else
+				return 0;
+		}
+
+		public String getColumnName(int col) {
+			return columnNames[col];
+		}
+
+		public Object getValueAt(int row, int col) {
+
+			try {
+				BasicRecipe r = data.get(row);
+				switch (col) {
+					case 0 :
+						return r.title;
+					case 1 :
+						return r.style;
+					case 2 :
+						return r.brewer;
+					case 3 :
+						return r.iteration;
+
+					default :
+						return "";
+
+				}
+			} catch (Exception e) {
+			};
+			return "";
+		}
+
+		/*
+		 * JTable uses this method to determine the default renderer/ editor for
+		 * each cell. If we didn't implement this method, then the last column
+		 * would contain text ("true"/"false"), rather than a check box.
+		 */
+
+		// public Class getColumnClass(int c) {
+		// return getValueAt(0, c).getClass();
+		// }
+	}
+
+}
