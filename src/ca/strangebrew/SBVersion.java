5f8714f46d05453a1bfbb90050bfe1d2bb728b64
Doug Edey <doug.edey@360pi.com>
2013-12-30 12:05:26 -0500

    Update version

--- src/ca/strangebrew/SBVersion.java
+++ src/ca/strangebrew/SBVersion.java
@@ -7,6 +7,6 @@
 		public final static String VERSION =
 			2 + "." + 1 + "." + 0;
 		public final static String BUILDDATE = "December 30, 2013";
-		public final static String BUILDNUMBER = "175";
+		public final static String BUILDNUMBER = "177";
 	}
 	
