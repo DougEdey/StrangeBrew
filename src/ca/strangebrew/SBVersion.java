74c2514c4e02f46e45260cc187ef798b4a78bbf8
Doug Edey <doug.edey+github@gmail.com>
2018-07-08 17:37:44 -0400

    New release

--- src/ca/strangebrew/SBVersion.java
+++ src/ca/strangebrew/SBVersion.java
@@ -6,7 +6,7 @@
 		/** JabaDex major release version */
 		public final static String VERSION =
 			2 + "." + 1 + "." + 0;
-		public final static String BUILDDATE = "July  7, 2018";
-		public final static String BUILDNUMBER = "285";
+		public final static String BUILDDATE = "July  8, 2018";
+		public final static String BUILDNUMBER = "286";
 	}
 	
